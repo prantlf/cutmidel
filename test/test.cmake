3bb9969684b718faf551c82f9a5f4e0e4826f4d8
Ferdinand Prantl <prantlf@gmail.com>
2020-11-15 10:36:59 +0100

    Test the master branch using GitHub actions

--- test/test.cmake
+++ test/test.cmake
@@ -1,23 +1,25 @@
 file(WRITE actual.txt "\$ ${EXE} Sources/private/cutmidel 5 10\n")
-execute_process(COMMAND ${EXE} Sources/private/cutmidel 5 10
+execute_process(COMMAND "${BIN}/${EXE}" Sources/private/cutmidel 5 10
   OUTPUT_VARIABLE OUTPUT)
 file(APPEND actual.txt "${OUTPUT}")
 
 file(APPEND actual.txt "\$ ${EXE} Sources/private/cutmidel 12 0 ''\n")
-execute_process(COMMAND ${EXE} Sources/private/cutmidel 12 0 ""
+execute_process(COMMAND "${BIN}/${EXE}" Sources/private/cutmidel 12 0 ""
   OUTPUT_VARIABLE OUTPUT)
 file(APPEND actual.txt "${OUTPUT}")
 
 file(APPEND actual.txt "\$ ${EXE} Sources/private/cutmidel 0 12 ..\n")
-execute_process(COMMAND ${EXE} Sources/private/cutmidel 0 12 ..
+execute_process(COMMAND "${BIN}/${EXE}" Sources/private/cutmidel 0 12 ..
   OUTPUT_VARIABLE OUTPUT)
 file(APPEND actual.txt "${OUTPUT}")
 
-execute_process(COMMAND ${CMAKE_COMMAND} -E compare_files "${SRC}/test/expected.txt" "${BIN}/actual.txt"
+execute_process(COMMAND diff -u "${SRC}/test/expected.txt" "${BIN}/actual.txt"
+  OUTPUT_VARIABLE OUTPUT
   RESULT_VARIABLE RESULT)
 if(RESULT EQUAL 0)
   message(STATUS "The actual test output matches the expected ooe.")
 elseif(RESULT EQUAL 1)
+  message("${OUTPUT}")
   message(SEND_ERROR "The actual test output differs from the expected one.")
 else()
   message(SEND_ERROR "Comparing the actual and expected test output failed.")
